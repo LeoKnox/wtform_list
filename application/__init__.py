101cc93de8f17be50aed284d821a4af74ef39b50
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-26 14:27:41 -0700

    Add files via upload

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from fask_mongoengine import MongoEngine
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from application import app
+from application import app, db
 from flask import render_template
 
 @app.route("/")
@@ -6,6 +6,13 @@ from flask import render_template
 def index():
     return render_template("index.html", nav_index="active")
 
+class ListOne(db.Document):
+    list_id     =   db.IntField( unique=True )
+    item_one    =   db.StringField()
+
 @app.route("/list")
 def list():
-    return render_template("list.html", nav_list="active")
+    #ListOne(list_id = 1, item_one="Red").save()
+    #ListOne(list_id = 2, item_one="Blue").save()
+    items = ListOne.objects.all()
+    return render_template("list.html", items=items, nav_list="active")
