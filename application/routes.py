06d18637e685950cfa505b36899f9fe609cdbdef
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-26 17:50:08 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -6,6 +6,10 @@ from flask import render_template
 def index():
     return render_template("index.html", nav_index="active")
 
+class list_item(db.Document):
+    li_id       =   db.IntField( unique=True )
+    li_data     =   db.StringField()
+
 class ListOne(db.Document):
     list_id     =   db.IntField( unique=True )
     item_one    =   db.StringField()
@@ -13,11 +17,14 @@ class ListOne(db.Document):
 
 @app.route("/list")
 def list():
-    #ListOne(list_id = 1, item_one="Red").save()
-    #ListOne(list_id = 2, item_one="Blue").save()
-    #ListOne(list_id = 3, item_one="Blue", list_list=[1,2]).save()
-    #ListOne.objects(list_id=3).update_one(list_list=[3])
-    ListOne.objects(list_id=3).update_one(push__list_list=4)
-    ListOne.objects(list_id=3).update(pull__list_list=3)
     items = ListOne.objects.all()
+    x = list_item(li_id=1, li_data="fly")
+    print(x.li_data)
     return render_template("list.html", items=items, nav_list="active")
+
+#ListOne(list_id = 1, item_one="Red").save()
+#ListOne(list_id = 2, item_one="Blue").save()
+#ListOne(list_id = 3, item_one="Blue", list_list=[1,2]).save()
+#ListOne.objects(list_id=3).update_one(list_list=[3])
+#ListOne.objects(list_id=3).update_one(push__list_list=4)
+#ListOne.objects(list_id=3).update(pull__list_list=3)
